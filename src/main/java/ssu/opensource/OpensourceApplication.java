83ee3363f0ced4f8498f2e0942a70721d705e160
choyeongju <jjudy1301@naver.com>
2024-11-11 15:28:24 +0900

    :sparkles: [feat] #4 의존성 추가

--- src/main/java/ssu/opensource/OpensourceApplication.java
+++ src/main/java/ssu/opensource/OpensourceApplication.java
@@ -2,8 +2,10 @@ package ssu.opensource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class OpensourceApplication {
 
 	public static void main(String[] args) {
