3a56212539fb3de2be623eb852549a62a59c79c1
choyeongju <jjudy1301@naver.com>
2024-11-15 19:25:41 +0900

    :sparkles: [feat] #11 unique 제약조건 추가

--- src/main/java/ssu/opensource/domain/User.java
+++ src/main/java/ssu/opensource/domain/User.java
@@ -21,7 +21,7 @@ public class User {
     @Column(name="name", nullable = false)
     private String name;
 
-    @Column(name="email", nullable = false)
+    @Column(name="email", nullable = false, unique = true)
     private String email;
 
     @Column(name="serial_id", nullable = false, unique = true)
