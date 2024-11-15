aea6ce0a681c30bf500bb678075e59dce1b82379
choyeongju <jjudy1301@naver.com>
2024-11-15 19:45:41 +0900

    :sparkles: [feat] #14 TaskStatus 엔티티 추가

---
+++ src/main/java/ssu/opensource/service/taskStatus/TaskStatusService.java
@@ -0,0 +1,9 @@
+package ssu.opensource.service.taskStatus;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TaskStatusService {
+}
