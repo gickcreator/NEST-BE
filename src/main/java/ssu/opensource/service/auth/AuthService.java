c61dc0f82dc1f539f6ccb577d5a06941b6ed5f5a
choyeongju <jjudy1301@naver.com>
2024-11-26 00:54:14 +0900

    :sparkles: [feat] Auth

--- src/main/java/ssu/opensource/service/auth/AuthService.java
+++ src/main/java/ssu/opensource/service/auth/AuthService.java
@@ -10,10 +10,12 @@ import ssu.opensource.feign.google.GoogleAuthClient;
 import ssu.opensource.feign.google.GoogleInfoClient;
 import ssu.opensource.feign.google.GoogleTokenResponse;
 import ssu.opensource.feign.google.GoogleUserInfoResponse;
+import ssu.opensource.service.google.GoogleService;
 import ssu.opensource.service.token.TokenRemover;
 import ssu.opensource.service.token.TokenRetriever;
 import ssu.opensource.service.token.TokenSaver;
 import ssu.opensource.service.user.UserRetriever;
+import ssu.opensource.service.user.UserUpdater;
 import ssu.opensource.utils.JwtUtil;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Value;
@@ -34,48 +36,47 @@ public class AuthService {
     @Value("${spring.security.oauth2.client.registration.google.redirect-uri}")
     private String redirectUrl;
 
-    private final GoogleAuthClient googleAuthClient;
-    private final GoogleInfoClient googleInfoClient;
     private final JwtUtil jwtUtil;
     private final TokenRetriever tokenRetriever;
     private final UserRetriever userRetriever;
+    private final UserUpdater userUpdater;
     private final TokenSaver tokenSaver;
     private final TokenRemover tokenRemover;
+    private final GoogleService googleService;
 
     @Transactional
-    public JwtTokensDto reissueToken(final String refreshToken){
-        String token = refreshToken.substring("Bearer ".length());
-        Long userId = tokenRetriever.findMemberIdByRefreshToken(token);
+    public JwtTokensDto reissueToken(final Long userId){
         JwtTokensDto tokensDto = jwtUtil.generateTokens(userId);
-        tokenSaver.save(Token.builder().id(userId).refreshToken(tokensDto.refreshToken()).build());
+
+        String userKey = userId.toString() + ":" + tokensDto.refreshToken();
+        tokenSaver.save(Token.builder().id(userKey).refreshToken(tokensDto.refreshToken()).build());
         return tokensDto;
     }
 
     @Transactional
-    public void logout(final Long userId){
-        Token refreshToken = tokenRetriever.findById(userId);
-        tokenRemover.deleteToken(refreshToken);
+    public void logout(final Long userId, final String refreshToken){
+        Token token = tokenRetriever.findById(userId, refreshToken);
+        tokenRemover.deleteToken(token);
     }
 
     @Transactional
-    public JwtTokensDto googleLogin(final String code)
-            throws IOException{
-        log.info("{}", code);
-        GoogleTokenResponse googleTokenResponse = googleAuthClient.googleAuth( // 구글 인증 API 호출
-                code, // 클라이언트에서 받은 인증 코드
-                //구글 API에 필요한 클라이언트 정보
-                clientId,
-                clientSecret,
-                redirectUrl,
-                //권한 부여 코드 타입
-                "authorization_code"
+    public JwtTokensDto googleLogin(final String code) {
+        GoogleTokenResponse googleTokenResponse = googleService.getToken(code, clientId, clientSecret, redirectUrl);
+        GoogleUserInfoResponse googleUserInfoResponse = googleService.getUserInfo(googleTokenResponse.accessToken());
+        User user = userRetriever.findBySerialIdAndEmailOrGet(
+                googleUserInfoResponse.sub(),
+                googleUserInfoResponse.givenName(),
+                googleUserInfoResponse.familyName(),
+                googleUserInfoResponse.picture(),
+                googleUserInfoResponse.email()
         );
-        GoogleUserInfoResponse googleUserInfoResponse = googleInfoClient.googleInfo( // 구글 사용자 정보 API를 호출
-                "Bearer " + googleTokenResponse.accessToken());
-
-        User user = userRetriever.findBySerialIdAndEmailOrGet(googleUserInfoResponse.sub(), googleUserInfoResponse.name(), googleUserInfoResponse.email());
+        if (user.getFamilyName() == null && user.getGivenName() == null){
+            userUpdater.updateName(user, googleUserInfoResponse.givenName(), googleUserInfoResponse.familyName());
+        }
         JwtTokensDto jwtTokensDto = jwtUtil.generateTokens(user.getId());
-        tokenSaver.save(Token.builder().id(user.getId()).refreshToken(jwtTokensDto.refreshToken()).build());
+
+        String userKey = user.getId() + ":" + jwtTokensDto.refreshToken();
+        tokenSaver.save(Token.builder().id(userKey).refreshToken(jwtTokensDto.refreshToken()).build());
         return jwtTokensDto;
     }
 }
