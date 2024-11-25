f06ada366e50efe9fdc58f2a309e2a0e646603e2
choyeongju <jjudy1301@naver.com>
2024-11-26 00:55:43 +0900

    :sparkles: [feat] Google 인증

--- src/main/java/ssu/opensource/feign/google/GoogleTokenResponse.java
+++ src/main/java/ssu/opensource/feign/google/GoogleTokenResponse.java
@@ -8,8 +8,4 @@ public record GoogleTokenResponse(
         String accessToken,
         String refreshToken
 ) {
-    public static GoogleTokenResponse of(String accessToken, String refreshToken) {
-        return new GoogleTokenResponse(accessToken, refreshToken);
-    }
-
 }
