48f25bef2fb746757f39f408962cb5dc964aa744
choyeongju <jjudy1301@naver.com>
2024-11-25 22:52:08 +0900

    :sparkles: [feat] feignClient

--- src/main/java/ssu/opensource/feign/google/GoogleInfoClient.java
+++ src/main/java/ssu/opensource/feign/google/GoogleInfoClient.java
@@ -3,11 +3,13 @@ package ssu.opensource.feign.google;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import ssu.opensource.constant.GoogleConstant;
 
-@FeignClient(name="GoogleInfoClient", url = "https://www.googleapis.com/oauth2/v3/userinfo")
+@FeignClient(name="GoogleInfoClient", url = GoogleConstant.GOOGLE_USER_INFO_URL)
 public interface GoogleInfoClient {
     @GetMapping
     GoogleUserInfoResponse googleInfo(
-            @RequestHeader("Authorization") String accessToken
+            @RequestHeader(GoogleConstant.AUTHORIZATION) final String accessToken
     );
+
 }
