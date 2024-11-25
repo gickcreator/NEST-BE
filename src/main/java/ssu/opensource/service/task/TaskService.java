557856bb9f015f9dc51aee437d794355250745e0
choyeongju <jjudy1301@naver.com>
2024-11-25 16:08:29 +0900

    :sparkles: [feat] Staging Area Task 생성 API 구현

---
+++ src/main/java/ssu/opensource/service/task/TaskService.java
@@ -0,0 +1,49 @@
+package ssu.opensource.service.task;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ssu.opensource.domain.Task;
+import ssu.opensource.domain.User;
+import ssu.opensource.dto.task.TaskCreateDto;
+import ssu.opensource.service.user.UserRetriever;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class TaskService {
+
+    private final TaskUpdater taskUpdater;
+    private final TaskRetriever taskRetriever;
+    private final TaskSaver taskSaver;
+    private final TaskRemover taskRemover;
+
+    private final UserRetriever userRetriever;
+
+    // Staging Area Task 생성 API
+    @Transactional
+    public Task createTask(final Long userId, final TaskCreateDto taskCreateDto) {
+        User user = userRetriever.findByUserId(userId);
+
+        LocalDate deadLineDate = null;
+        LocalTime deadLineTime = null;
+
+        if (taskCreateDto.deadLine() != null) {
+            deadLineDate = taskCreateDto.deadLine().date();
+            deadLineTime = taskCreateDto.deadLine().time();
+        }
+
+        Task task = Task.builder()
+                .user(user)
+                .name(taskCreateDto.name())
+                .deadLineDate(deadLineDate)
+                .deadLineTime(deadLineTime)
+                .build();
+        return taskSaver.save(task);
+    }
+
+}
--- src/main/java/ssu/opensource/service/user/UserRetriever.java
+++ src/main/java/ssu/opensource/service/user/UserRetriever.java
@@ -3,12 +3,21 @@ package ssu.opensource.service.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import ssu.opensource.domain.User;
+import ssu.opensource.exception.NotFoundException;
+import ssu.opensource.exception.code.NotFoundErrorCode;
 import ssu.opensource.repository.UserRepository;
 
 @Component
 @RequiredArgsConstructor
 public class UserRetriever {
+
     private final UserRepository userRepository;
+
+    public User findByUserId(final Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException(NotFoundErrorCode.NOT_FOUND_USER));
+    }
+
     public User findBySerialIdAndEmailOrGet(final String serialId, final String name, final String email){
         return userRepository.findBySerialIdAndEmail(serialId, email).orElseGet(
                 ()-> userRepository.save(User.builder().serialId(serialId).name(name).email(email).build())
