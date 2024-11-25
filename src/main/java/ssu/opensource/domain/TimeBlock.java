d17890d01f69f3c05b2f6912e5e44bafedd58947
choyeongju <jjudy1301@naver.com>
2024-11-25 22:23:29 +0900

    :sparkles: [feat] 필요한 도메인 구현

--- src/main/java/ssu/opensource/domain/TimeBlock.java
+++ src/main/java/ssu/opensource/domain/TimeBlock.java
@@ -17,14 +17,11 @@ public class TimeBlock {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name="date", nullable = false)
-    private LocalDate date;
-
     @Column(name="start_time", nullable = false)
-    private String startTime;
+    private LocalDateTime startTime;
 
     @Column(name="end_time", nullable = false)
-    private String endTime;
+    private LocalDateTime endTime;
 
     @Column(name="created_at", nullable = false)
     private LocalDateTime createdAt;
@@ -32,22 +29,20 @@ public class TimeBlock {
     @Column(name="updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
-    @ManyToOne(targetEntity= Task.class, fetch= FetchType.LAZY)
-    @JoinColumn(name="task_id", nullable = false)
-    private Task task;
+    @OneToOne(targetEntity = TaskStatus.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_status_id", nullable = false)
+    private TaskStatus taskStatus;
 
     @Builder
-    public TimeBlock(LocalDate date, String startTime, String endTime, Task task) {
-        this.date = date;
+    public TimeBlock(LocalDateTime startTime, LocalDateTime endTime, TaskStatus taskStatus) {
         this.startTime = startTime;
         this.endTime = endTime;
-        this.task = task;
+        this.taskStatus = taskStatus;
         this.createdAt = LocalDateTime.now();
         this.updatedAt = LocalDateTime.now();
     }
 
-    @Builder
-    public void updateTimeBlock(String startTime, String endTime) {
+    public void updateTimeBlock(LocalDateTime startTime, LocalDateTime endTime) {
         this.startTime = startTime;
         this.endTime = endTime;
         this.updatedAt = LocalDateTime.now();
