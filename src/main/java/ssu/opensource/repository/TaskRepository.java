a03412a9732204ca4a072d1690d2acc721e5e03b
choyeongju <jjudy1301@naver.com>
2024-11-26 00:55:35 +0900

    :sparkles: [feat] Task

--- src/main/java/ssu/opensource/repository/TaskRepository.java
+++ src/main/java/ssu/opensource/repository/TaskRepository.java
@@ -1,12 +1,69 @@
 package ssu.opensource.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ssu.opensource.domain.Task;
 import ssu.opensource.domain.User;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-
     Optional<Task> findByUserAndId(final User user, final Long aLong);
+
+    @Query(value="select t from Task t where t.user = :user " +
+            "and exists (select tb from TimeBlock tb " +
+            "where tb.taskStatus.task = t and tb.startTime between :startTime and :endTime " +
+            "and tb.endTime between :startTime and :endTime)")
+    List<Task> findAllByUserAndTimeBlocks(final User user, LocalDateTime startTime, LocalDateTime endTime);
+
+    List<Task> findAllByUserAndAssignedDateIsNullOrderByCreatedAtDesc(final User user);
+
+    List<Task> findAllByUserAndAssignedDateIsNullOrderByCreatedAtAsc(final User user);
+
+    @Query(
+            value = "select * from task t where t.user_id = :userId " +
+                    "AND t.assigned_date is null " +
+                    "order by abs(current_date - t.dead_line_date) asc nulls last, " +
+                    "abs(extract(epoch from current_time - t.dead_line_time)) asc nulls last"
+            , nativeQuery = true
+    )
+    List<Task> findAllByUserAndAssignedDateIsNullOrderByTimeDiffAsc(final Long userId);
+
+    @Query(
+            value = "select * from task t where t.user_id = :userId " +
+                    "AND t.assigned_date is null " +
+                    "order by abs(current_date - t.dead_line_date) desc nulls last, " +
+                    "abs(extract(epoch from current_time - t.dead_line_time)) desc nulls last"
+            , nativeQuery = true
+    )
+    List<Task> findAllByUserAndAssignedDateIsNullOrderByTimeDiffDesc(final Long userId);
+
+    @Query(
+            value = "select * from task t where t.user_id = :userId " +
+                    "AND t.assigned_date is null " +
+                    "AND t.dead_line_date <= current_date + interval '2 days' " +
+                    "order by t.dead_line_date asc nulls last, t.dead_line_time asc nulls last"
+            ,nativeQuery = true
+    )
+    List<Task> findAllUpcomingTasksByUserWitAssignedStatus(final Long userId);
+
+    @Query(
+            value = "select * from task t where t.user_id = :userId " +
+                    "AND t.status = 'DEFERRED' AND t.assigned_date is null " +
+                    "order by t.dead_line_date nulls last, t.dead_line_time nulls last"
+            ,nativeQuery = true
+    )
+    List<Task> findAllDeferredTasksByUserWithStatus(final Long userId);
+
+    // 설정한 기간 내의 assigned 된 일들 찾는 거
+    @Query(
+            value = "select count(*) from task t where t.user_id = :userId " +
+                    "AND t.assigned_date BETWEEN :startDate AND :endDate"
+            ,nativeQuery = true
+    )
+    Integer countAllAssignedTasksInPeriod(final Long userId, final LocalDate startDate, final LocalDate endDate);
 }
+
--- src/main/java/ssu/opensource/service/task/TaskRetriever.java
+++ src/main/java/ssu/opensource/service/task/TaskRetriever.java
@@ -8,14 +8,42 @@ import ssu.opensource.exception.NotFoundException;
 import ssu.opensource.exception.code.NotFoundErrorCode;
 import ssu.opensource.repository.TaskRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 public class TaskRetriever {
-
     private final TaskRepository taskRepository;
 
     public Task findByUserAndId(final User user, final Long taskId) {
         return taskRepository.findByUserAndId(user, taskId)
                 .orElseThrow(() -> new NotFoundException(NotFoundErrorCode.NOT_FOUND_TASK));
+    }
+
+    public Task findById(final Long id) {
+        return taskRepository.findById(id).orElseThrow(
+                () -> new NotFoundException(NotFoundErrorCode.NOT_FOUND_TASK)
+        );
+    }
+
+    public List<Task> findAllByUserAndTimeBlocks(User user, LocalDateTime startTime, LocalDateTime endTime) {
+        return taskRepository.findAllByUserAndTimeBlocks(user, startTime, endTime);
+    }
+
+    public List<Task> findAllByUserAndAssignedDateIsNullOrderByCreatedAtDesc(final User user) {
+        return taskRepository.findAllByUserAndAssignedDateIsNullOrderByCreatedAtDesc(user);
+    }
+
+    public List<Task> findAllByUserAndAssignedDateIsNullOrderByCreatedAtAsc(final User user) {
+        return taskRepository.findAllByUserAndAssignedDateIsNullOrderByCreatedAtAsc(user);
+    }
+
+    public List<Task> findAllByUserAndAssignedDateIsNullOrderByTimeDiffAsc(final User user) {
+        return taskRepository.findAllByUserAndAssignedDateIsNullOrderByTimeDiffAsc(user.getId());
+    }
+
+    public List<Task> findAllByUserAndAssignedDateIsNullOrderByTimeDiffDesc(final User user) {
+        return taskRepository.findAllByUserAndAssignedDateIsNullOrderByTimeDiffDesc(user.getId());
     }
 }
