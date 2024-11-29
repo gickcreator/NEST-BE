10e1e1c88d5391d2c6e31ea32d81f54efb4a9cb6
choyeongju <jjudy1301@naver.com>
2024-11-29 16:10:04 +0900

    :recycle: [fix] whitelist 경로 수정

--- src/main/java/ssu/opensource/constant/AuthConstant.java
+++ src/main/java/ssu/opensource/constant/AuthConstant.java
@@ -6,14 +6,10 @@ public class AuthConstant {
     public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String ANONYMOUS_USER = "anonymousUser";
     public static final String[] AUTH_WHITELIST = {
-            "/api/**",
-            "/v3/api-docs/**",
             "/actuator/health",
             "/api/auth/login/google/**",
             "/api/auth/login/google",
             "/api/test/token/**",
-            "/login/google",
-            "/auth/logout",
     };
     private AuthConstant() {
     }
